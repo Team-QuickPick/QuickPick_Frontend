8bae11a7224753f5de364612dd445b341c067a3e
kjy <rla1014ss@gmail.com>
2023-04-05 15:29:24 +0900

    feat: page 추가, route 설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./Home.module.scss";
 
 export default function Home() {
-  return <div></div>;
+  return <div className={styles.container}>컨테이너 세팅</div>;
 }

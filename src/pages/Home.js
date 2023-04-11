f45e8d26dbb63104dc3aa0eedfc80af823b6108c
leedg <leedg919@gmail.com>
2023-04-11 11:08:03 +0900

    main page get

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,161 +5,161 @@ import HomeHeader from "../components/HomeHeader";
 
 import axios from 'axios';
 
-const dummyData = [
-  {
-    "pk": 21,
-    "name": "닥터지 레드블레미쉬 클리어 수딩크림",
-    "price": 22600,
-    "image": null,
-    "product_location": [
-      1,
-      2
-    ]
-  },
-  {
-    "pk": 22,
-    "name": "필리밀리 쿠션 팡팡퍼프 5P",
-    "price": 8500,
-    "image": null,
-    "product_location": [
-      1,
-      2,
-      3
-    ]
-  },
-  {
-    "pk": 23,
-    "name": "클린 웜코튼 30ml",
-    "price": 27000,
-    "image": null,
-    "product_location": [
-      1,
-      3,
-      4
-    ]
-  },
-  {
-    "pk": 24,
-    "name": "웨이크메이크 마스카라",
-    "price": 12000,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 25,
-    "name": "센카 퍼펙트 휩 페이셜 워시 2입",
-    "price": 11500,
-    "image": null,
-    "product_location": [
-      1,
-      5
-    ]
-  },
-  {
-    "pk": 26,
-    "name": "트리헛 시어 슈가 스크럽",
-    "price": 13000,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 27,
-    "name": "유시몰 화이트닝 치약 106g",
-    "price": 12900,
-    "image": null,
-    "product_location": [
-      1,
-      2
-    ]
-  },
-  {
-    "pk": 28,
-    "name": "라보에이치 샴푸",
-    "price": 27600,
-    "image": null,
-    "product_location": [
-      1,
-      4
-    ]
-  },
-  {
-    "pk": 29,
-    "name": "어노브 딥 데미지 트리트먼트 EX",
-    "price": 14900,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 30,
-    "name": "필리밀리 뿌리 볼륨 롤 브러시",
-    "price": 11500,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 31,
-    "name": "포맨트 시그니처 퍼퓸 50ml",
-    "price": 49000,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 32,
-    "name": "투쿨포스쿨 바이로댕 쉐이딩 기획",
-    "price": 16000,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 33,
-    "name": "바닐라코 클린잇제로 클렌징 밤",
-    "price": 18000,
-    "image": null,
-    "product_location": [
-      1,
-      2
-    ]
-  },
-  {
-    "pk": 34,
-    "name": "헉슬리 퍼퓸 핸드크림",
-    "price": 15000,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 35,
-    "name": "플랙커스 마이크로민트 치실 36개입",
-    "price": 3700,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  },
-  {
-    "pk": 36,
-    "name": "달바 화이트 트러플 퍼스트 스프레이 세럼 100ml",
-    "price": 29900,
-    "image": null,
-    "product_location": [
-      1
-    ]
-  }
-];
+// const dummyData = [
+  // {
+  //   "pk": 21,
+  //   "name": "닥터지 레드블레미쉬 클리어 수딩크림",
+  //   "price": 22600,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     2
+  //   ]
+  // },
+  // {
+  //   "pk": 22,
+  //   "name": "필리밀리 쿠션 팡팡퍼프 5P",
+  //   "price": 8500,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     2,
+  //     3
+  //   ]
+  // },
+  // {
+  //   "pk": 23,
+  //   "name": "클린 웜코튼 30ml",
+  //   "price": 27000,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     3,
+  //     4
+  //   ]
+  // },
+  // {
+  //   "pk": 24,
+  //   "name": "웨이크메이크 마스카라",
+  //   "price": 12000,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 25,
+  //   "name": "센카 퍼펙트 휩 페이셜 워시 2입",
+  //   "price": 11500,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     5
+  //   ]
+  // },
+  // {
+  //   "pk": 26,
+  //   "name": "트리헛 시어 슈가 스크럽",
+  //   "price": 13000,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 27,
+  //   "name": "유시몰 화이트닝 치약 106g",
+  //   "price": 12900,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     2
+  //   ]
+  // },
+  // {
+  //   "pk": 28,
+  //   "name": "라보에이치 샴푸",
+  //   "price": 27600,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     4
+  //   ]
+  // },
+  // {
+  //   "pk": 29,
+  //   "name": "어노브 딥 데미지 트리트먼트 EX",
+  //   "price": 14900,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 30,
+  //   "name": "필리밀리 뿌리 볼륨 롤 브러시",
+  //   "price": 11500,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 31,
+  //   "name": "포맨트 시그니처 퍼퓸 50ml",
+  //   "price": 49000,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 32,
+  //   "name": "투쿨포스쿨 바이로댕 쉐이딩 기획",
+  //   "price": 16000,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 33,
+  //   "name": "바닐라코 클린잇제로 클렌징 밤",
+  //   "price": 18000,
+  //   "image": null,
+  //   "product_location": [
+  //     1,
+  //     2
+  //   ]
+  // },
+  // {
+  //   "pk": 34,
+  //   "name": "헉슬리 퍼퓸 핸드크림",
+  //   "price": 15000,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 35,
+  //   "name": "플랙커스 마이크로민트 치실 36개입",
+  //   "price": 3700,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // },
+  // {
+  //   "pk": 36,
+  //   "name": "달바 화이트 트러플 퍼스트 스프레이 세럼 100ml",
+  //   "price": 29900,
+  //   "image": null,
+  //   "product_location": [
+  //     1
+  //   ]
+  // }
+// ];
 
 export default function Home() {
   // 광고 캐러셀
@@ -201,7 +201,7 @@ export default function Home() {
       }
     } catch (error) {
       console.log("인기 상품 데이터 조회 실패",error.message);
-      setPopularProductList(dummyData);
+      // setPopularProductList(dummyData);
     }
   };
 

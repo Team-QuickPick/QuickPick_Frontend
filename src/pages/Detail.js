ab41009c8588671618a8c6f8a35d8e796dbc11a0
kjy <rla1014ss@gmail.com>
2023-04-12 18:43:02 +0900

    feat: detail page 카테고리에 따른 이미지 출력 ex) 선택한 상품의 product_number가 A로 시작하면 A가 색칠된 이미지

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -16,12 +16,16 @@ export default function Detail() {
       .get(`http://127.0.0.1:8000/api/v1/products/${id}`)
       .then((response) => {
         const productData = {
-          id: response.data.pk,
+          id: response.data.id,
           name: response.data.name,
           price: response.data.price,
           image: response.data.image,
+          category: response.data.product_category,
+          number: response.data.product_number,
+          categoryimage: response.data.product_number[0],
         };
         setProduct(productData);
+        console.log(productData);
       });
   }, [id]);
 
@@ -31,9 +35,16 @@ export default function Detail() {
       <div className={styles.container}>
         <div>
           <h1>상품 상세 정보</h1>
-          {/* <img src={product.image} alt={product.name} /> */}
+          <img src={product.image} alt={product.name} />
+
+          <img
+            src={`/img/detail/${product.categoryimage}.png`}
+            alt={product.categoryimage}
+          />
           <h2>{product.name}</h2>
           <h3>{product.price}</h3>
+          <h3>{product.category}</h3>
+          <h3>{product.number}</h3>
         </div>
       </div>
       <Navbar />

69009cf85df9239a5224e48610c90ab24f9f68a4
kjy <rla1014ss@gmail.com>
2023-04-14 17:08:47 +0900

    feat. 검색 기능 매장 필터 기능 아직 구현 안 된 상태

--- src/components/DetailHeader.js
+++ src/components/DetailHeader.js
@@ -1,13 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./DetailHeader.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const DetailHeader = () => {
+  const location = useLocation();
   const navigate = useNavigate();
 
   const handleGoBack = () => {
-    navigate(-1);
+    navigate(-1, { state: { ...location.state } });
   };
 
   return (
--- src/components/SelectStore.js
+++ src/components/SelectStore.js
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./SelectStore.module.scss";
 
-const SelectStore = ({ onStoreSelect }) => {
+const SelectStore = ({ onStoreSelected }) => {
   const [stores, setStores] = useState([]);
+
+  const handleStoreSelect = (event) => {
+    const storeId = event.target.value;
+    if (storeId) {
+      onStoreSelected(storeId);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,18 +21,13 @@ const SelectStore = ({ onStoreSelect }) => {
         return acc;
       }, {});
       setStores(stores);
-      console.log(stores);
     };
     fetchData();
   }, []);
 
-  const handleChange = (event) => {
-    onStoreSelect(event.target.value);
-  };
-
   return (
     <div className={styles.selectStoreWrapper}>
-      <select onChange={handleChange}>
+      <select onChange={handleStoreSelect}>
         <option value="">매장을 선택하세요</option>
         {Object.entries(stores).map(([key, value]) => (
           <option key={key} value={key}>

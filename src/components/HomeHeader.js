cfb55465a3382aed46d92b4ff3f0bebdc6da5615
kjy <rla1014ss@gmail.com>
2023-04-24 15:01:44 +0900

    0424

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -5,13 +5,6 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-function handleLogin() {
-  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  window.location.href = KAKAO_AUTH_URL;
-}
-
 export default class HomeHeader extends Component {
   render() {
     return (
@@ -19,14 +12,7 @@ export default class HomeHeader extends Component {
         <div className={styles.logo}>
           <img className={styles.logoImg} src="img/Logo.png" />
         </div>
-        <div className={styles.user}>
-          <button onClick={handleLogin}>카카오로 시작하기</button>
-          {/* <div className={styles.userIcon}>
-            <Link to="/">
-              <FontAwesomeIcon icon={faUser} size="lg" color="black" />
-            </Link>
-          </div> */}
-        </div>
+        <div className={styles.user}>로그인</div>
       </header>
     );
   }

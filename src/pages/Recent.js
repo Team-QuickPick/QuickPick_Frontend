bc670719b8c3e100c5df39d53ccdeea9c35fc63f
kjy <rla1014ss@gmail.com>
2023-04-25 11:42:34 +0900

    feat: login page

--- src/pages/Recent.js
+++ src/pages/Recent.js
@@ -18,17 +18,23 @@ export default function Recent() {
       <RecentHeader />
       <div className={styles.container}>
         <h2>최근 본 상품</h2>
-        {recentProducts.map((product) => (
-          // <Link to={`/detail/${product.id}`} key={product.id}>
-          <div className={styles.product}>
-            <img src={product.image} alt={product.name} />
-            <div className={styles.productInfo}>
-              <div className={styles.productName}>{product.name}</div>
-              <div className={styles.productPrice}>{product.price}</div>
-            </div>
+        {recentProducts.length > 0 ? (
+          <div className={styles.productList}>
+            {recentProducts.map((product) => (
+              <Link to={`/detail/${product.id}`} key={product.id}>
+                <div className={styles.product}>
+                  <img src={product.image} alt={product.name} />
+                  <div className={styles.productInfo}>
+                    <div className={styles.productName}>{product.name}</div>
+                    <div className={styles.productPrice}>{product.price}원</div>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
-          // </Link>
-        ))}
+        ) : (
+          <div className={styles.noProducts}>최근 본 상품이 없습니다.</div>
+        )}
       </div>
       <Navbar />
     </>

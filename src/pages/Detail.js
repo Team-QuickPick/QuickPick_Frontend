aa7d15d41d71bb1fcd4d2863f9a625936ff3d96e
leedg <leedg919@gmail.com>
2023-04-28 10:25:08 +0900

    product_category

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router-dom";
 
 import axios from "axios";
@@ -11,8 +10,13 @@ export default function Detail() {
   const { id } = useParams();
   const location = useLocation();
   const [product, setProduct] = useState(location.state?.product || {});
+  const [recommendedProducts, setRecommendedProducts] = useState([]);
+  
 
   useEffect(() => {
+    console.log('id--------------------',id)
+    console.log('location--------------------',location)
+    console.log('------------------',product);
     async function getProductData() {
       try {
         const response = await axios.get(
@@ -28,6 +32,15 @@ export default function Detail() {
           categoryimage: response.data.product_number[0],
         };
         setProduct(productData);
+
+        // Get recommended products with the same category
+        const recommendedResponse = await axios.get(
+          // `http://127.0.0.1:8000/api/v1/products?category=${productData.category}&limit=6`
+          `http://127.0.0.1:8000/api/v1/products/`
+        );
+        const recommendedProducts = recommendedResponse.data.slice(0,6);
+        setRecommendedProducts(recommendedProducts);
+
 
         // 클릭한 상품 정보를 localStorage에 저장
         const recentProducts = JSON.parse(
@@ -61,10 +74,13 @@ export default function Detail() {
     }
   }, [id, product.id]);
 
+  
+
   return (
     <>
       <DetailHeader />
       <div className={styles.container}>
+        {/* 검색 결과 */}
         <div className={styles.product}>
           <h3 className={styles.productCategory}>{product.category}</h3>
           <img
@@ -80,6 +96,21 @@ export default function Detail() {
           <h3 className={styles.productName}>{product.name}</h3>
           <h3 className={styles.productPrice}>{product.price}원</h3>
         </div>
+
+        {recommendedProducts.length > 0 && (
+          <div className={styles.recommended}>
+            <h3>추천 상품</h3>
+            <div className={styles.products}>
+              {recommendedProducts.map((product) => (
+                <div key={product.id} className={styles.product}>
+                  <img src={product.image} alt={product.name} />
+                  <h3>{product.name}</h3>
+                  <h3>{product.price}원</h3>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
       <Navbar />
     </>

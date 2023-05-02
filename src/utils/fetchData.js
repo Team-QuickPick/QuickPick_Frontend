21deeeee36858808a222e31d92ed64beccf5d362
kjy <rla1014ss@gmail.com>
2023-05-02 13:59:16 +0900

    fetch data api 수정

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // 상품 데이터를 가져오는 함수
 export const fetchProducts = async () => {
-  const response = await axios.get("http://127.0.0.1:8000/api/v1/products/");
+  const response = await axios.get("http://115.85.183.154/api/v1/products/");
   return response.data || [];
 };
 
 // 매장 데이터를 가져오는 함수
 export const fetchStores = async () => {
-  const response = await axios.get("http://127.0.0.1:8000/api/v1/stores/");
+  const response = await axios.get("http://115.85.183.154/api/v1/stores/");
   return response.data;
 };

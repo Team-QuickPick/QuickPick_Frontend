dff40a0bc31a127b5224e884252282ab07f3f49a
kjy <rla1014ss@gmail.com>
2023-05-02 15:18:08 +0900

    Squashed commit of the following:
    commit 733e174a274c81ceedc5148dae5f93f17e51909a
    Author: kjy <rla1014ss@gmail.com>
    Date:   Tue May 2 15:13:30 2023 +0900
    
        최근 검색어 없을 때 ui 수정

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -111,12 +111,12 @@ export default function Search() {
             <div className={styles.noResults}>찾으시는 상품이 없습니다.</div>
           ) : (
             <>
-              <RecentSearches
-                setSearchTerm={setSearchTerm}
-                onSearchTermChange={handleSearchTermChange}
-                onSearchButtonClick={handleSearch}
-              />
-              <div>
+              <div className={styles.recentAndPopular}>
+                <RecentSearches
+                  setSearchTerm={setSearchTerm}
+                  onSearchTermChange={handleSearchTermChange}
+                  onSearchButtonClick={handleSearch}
+                />
                 <PopularSearches />
               </div>
             </>
